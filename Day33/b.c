79b9088fe7add5a6f48e7d65e054ef665e2fdbc4
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-09 19:05:52 +0530

    Initial Commit

---
+++ Day33/b.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+char *Mystrcat(char *str1,char *str2){
+    while(*str1!=0){
+        str1++;
+    }
+    while(*str2!='\0'){
+        *str1=*str2;
+        str1++;
+        str2++;
+    }
+    *str1='\0';
+    return str1;
+}
+void main(){
+    char str1[20]={'R','A','J','\0'};
+    char *str2="MADGULKAR";
+
+    puts(str1);
+    puts(str2);
+
+    Mystrcat(str1,str2);
+
+    puts(str1);
+    puts(str2);
+}
