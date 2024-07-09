d2d936aa4f97c3c112fcd90b23552a7cfafea961
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-09 22:19:19 +0530

    Initial Commit

---
+++ Day33/s.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(int argc,char *argv[]){
+        for (int i=0; i<argc;i++){
+                printf("%s\n",argv[i]);
+        }
+}
+
+
+
+
+/*
+./a.out
+raj
+pratik
+atharv
+jay
+tushar
+*/
