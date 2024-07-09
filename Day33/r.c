e67f7d34c4f2db3dc12125052e101242620ad08f
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-09 22:15:15 +0530

    Initial Commit

---
+++ Day33/r.c
@@ -0,0 +1,10 @@
+//Command line arguements
+#include<stdio.h>
+void main(int argc,char *argv[]){
+        printf("%s\n",argv[0]);
+        printf("%d\n",argc);
+}
+/*
+./a.out Raj Ganesh Atharv Pratik
+5
+*/
