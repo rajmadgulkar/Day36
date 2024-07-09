0ee02cf425aa0e75a0254928ce288a566b6fc162
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-09 18:57:52 +0530

    Initial Commit

---
+++ Day33/a.c
@@ -0,0 +1,20 @@
+//Using declaration
+#include<stdio.h>
+void main(){
+    char str1[20]={'R','A','J','\0'};
+    char *str2="MADGULKAR";
+
+    puts(str1);
+    puts(str2);
+
+    strcat(str1,str2);
+
+    puts(str1);
+    puts(str2);
+}
+/*
+RAJ
+MADGULKAR
+RAJMADGULKAR
+MADGULKAR
+*/
