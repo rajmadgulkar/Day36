ac182f37de8a98874a6586aa6c932ac8bb1b52a0
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-16 23:28:07 +0530

    Initial Commit

---
+++ Day35/h.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int **ptr=(int*)calloc(5,sizeof(int));
+    for (int i=0;i<5;i++){
+        *(ptr+i)=10+i;
+    }
+    for(int i=0;i<5;i++){
+        printf("%d\n",*(ptr+i));
+    }
+    int **ptr2=(int*)realloc(ptr,8);
+    for (int i=0;i<8;i++){
+        *(ptr2+i)=10+i;
+    }
+    for(int i=0;i<8;i++){
+        printf("%d\n",*(ptr+i));
+    }
+}
+/*
+10
+11
+12
+13
+14
+10
+11
+12
+13
+14
+0
+5177424
+651704008
+*/
